4ebe306d75b4de0b9606c2788a33cc25759f8463
atlas <contact@atlas-services.fr>
2020-10-10 22:37:57 +0200

    Correction bug taille image d'accueil + template cards : ajout de configuration

--- src/Form/Admin/ConfigType.php
+++ src/Form/Admin/ConfigType.php
@@ -63,11 +63,23 @@ class ConfigType extends AbstractBaseType
                 '90%' => '90%',
                 '100%' => '100%',
             ];
+            $options_pct_value = [
+                '10%' => '10',
+                '20%' => '20',
+                '30%' => '30',
+                '40%' => '40',
+                '50%' => '50',
+                '60%' => '60',
+                '70%' => '70',
+                '80%' => '80',
+                '90%' => '90%',
+                '100%' => '100',
+            ];
             switch ($code) {
                 // accueil
                 case 'accueil':
                     $choice = true;
-                    $options = $options_pct;
+                    $options = $options_pct_value;
                 break;
                 // nav_bar
                 case 'nav_bar':
