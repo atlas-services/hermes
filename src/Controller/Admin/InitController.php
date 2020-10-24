8e14fa7a8341d51c9a8f6916c02e05dd650f6f2a
atlas <contact@atlas-services.fr>
2020-10-24 18:23:29 +0200

    Admin : initialisation du site à partir ds templates libres "Vin d'honneur", "Qui sommes nous",...

--- src/Controller/Admin/InitController.php
+++ src/Controller/Admin/InitController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Config;
 use App\Entity\Menu;
+use App\Entity\Post;
+use App\Entity\Section;
 use App\Entity\Sheet;
 use App\Entity\Template;
 use App\Entity\User;
@@ -14,14 +16,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/{_locale}/admin/add-config")
+ * @Route("/{_locale}/admin/add")
  */
 class InitController extends AbstractController
 {
     /**
-     * @Route("/", name="add_config", methods={"GET"})
+     * @Route("/config/", name="add_config", methods={"GET"})
      */
-    public function new(): Response
+    public function newConfig(): Response
     {
         $configurations = $this->getParameter('init');
         try {
@@ -34,15 +36,26 @@ class InitController extends AbstractController
             $dbtemplate = $this->getDoctrine()
                 ->getRepository(Template::class)
                 ->findAll();
-            $this->newConfig($configurations, $dbuser, $dbconfig,$dbtemplate);
+            $this->addConfig($configurations, $dbuser, $dbconfig,$dbtemplate);
         }catch (\Exception $e){
 
         }
-        return $this->redirectToRoute('app_login');
+        return $this->redirectToRoute('admin_index');
 
     }
 
-    private function newConfig($configurations, $dbuser,$dbconfig,$dbtemplate){
+    /**
+     * @Route("/page/{libre}", name="add_page", methods={"GET"})
+     */
+    public function newPage($libre): Response
+    {
+
+        $this->addPage($libre);
+        return $this->redirectToRoute('admin_index');
+
+    }
+
+    private function addConfig($configurations, $dbuser,$dbconfig,$dbtemplate){
         $entityManager = $this->getDoctrine()->getManager();
         $dbuseremail = array_column($dbuser, 'email');
         $dbconfigcode = array_column($dbconfig, 'code');
@@ -93,6 +106,60 @@ class InitController extends AbstractController
             }
         }
         $entityManager->flush();
+    }
+
+    private function addPage($libre){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $content = $this->render('admin/exemple/base/'.$libre.'.html.twig', [])->getContent();
+        try {
+            $sheet = $this->getDoctrine()
+                ->getRepository(Sheet::class)
+                ->findBy(['code'=> 'new_sheet'.$libre]);
+            if(!$sheet){
+                // add sheet
+                $sheet = new Sheet();
+                $sheet->setCode($libre);
+                $sheet->setName(str_replace('-', ' ', $libre));
+                $sheet->setPosition(1);
+                $sheet->setSummary('Menu '.$libre);
+                $sheet->setSlug($libre);
+                // add menu
+                $menu = new Menu();
+                $menu->setCode($libre);
+                $menu->setName(str_replace('-', ' ', $libre));
+                $menu->setPosition(1);
+                $menu->setSlug($libre);
+                $menu->setSheet($sheet);
+                // add section
+                $template = $this->getDoctrine()
+                    ->getRepository(Template::class)
+                    ->findOneBy(['code'=> 'libre']);
+                $section = new Section();
+                $section->setName('section-'.$libre);
+                $section->setPosition(1);
+                $section->setMenu($menu);
+                $section->setTemplate($template);
+                // add Post
+                $post = new Post();
+                $post->setName($libre);
+                $post->setPosition(1);
+                $post->setSection($section);
+                $post->setContent($content);
+
+                $entityManager->persist($sheet);
+                $entityManager->persist($menu);
+                $entityManager->persist($section);
+                $entityManager->persist($post);
+            }
+
+        }catch (\Exception $e){
+            $this->addFlash('warning', $e->getMessage());
+            return $this->redirectToRoute('admin_index');
+        }
+        $entityManager->flush();
+        $this->addFlash('info', 'Page créée!');
+        return $this->redirectToRoute('admin_index');
     }
 
 
