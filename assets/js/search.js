2ded94d2ae44582284e282c6bf2a329eef37b03a
atlas <contact@atlas-services.fr>
2020-11-06 17:29:29 +0100

    correction bugs Mise en place recherche de texte dans les posts et le livre d'or

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,6 +8,11 @@ $('#search_icon').on("mouseover", e => {
     $('#search_bar').show(duration, easing);
 });
 
+$('#search_icon').on("click", e => {
+    $('#search_bar').css('visibility', 'visible');
+    $('#search_bar').show(duration, easing);
+});
+
 $('#search_bar').on("mouseout", e => {
     if ('' == $('#search_bar').val()){
         $('#search_bar').hide(duration, easing);
