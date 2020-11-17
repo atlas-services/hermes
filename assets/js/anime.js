765c05e8b4cc7b7cba1507a6e95590cb9f56eb16
atlas <contact@atlas-services.fr>
2020-11-17 14:41:35 +0100

    correctif responsive de folio4.html.twig

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -323,42 +323,42 @@ if ($(".logo-animation")[0]) {
 stagering
  */
 
-const images = document.querySelectorAll("span.staggering");
-
-images.forEach((image, index) => {
-    image.addEventListener("mouseover", e => {
-        anime({
-            targets: images,
-            autoplay: true,
-            opacity: [
-                { value: .5, easing: "easeOutSine", duration: 600 },
-                { value: 1, easing: "easeInOutQuad", duration: 900 }],
-
-            filter: [
-                { value: "blur(5px) grayscale(100%)", easing: "easeOutSine", duration: 700 },
-                { value: "blur(0px) grayscale(0%)", easing: "easeInOutQuad", duration: 1000 }],
-
-            borderRadius: [
-                { value: "50%", easing: "easeOutSine", duration: 700 },
-                { value: "0%", easing: "easeInOutQuad", duration: 1000 }],
-
-            scale: [
-                { value: 0.5, easing: "easeOutSine", duration: 600 },
-                { value: 1, easing: "easeInOutQuad", duration: 1100 }],
-
-            delay: anime.stagger(200, { grid: [5, 5], from: index }) });
-
-    });
-});
-
-// window.onload = () => document.querySelector('h1').remove();
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll("img.stagge").forEach(img => {
-        img.onerror = function () {
-            this.style.display = "none";
-        };
-    });
-});
+// const images = document.querySelectorAll("span.staggering");
+//
+// images.forEach((image, index) => {
+//     image.addEventListener("mouseover", e => {
+//         anime({
+//             targets: images,
+//             autoplay: true,
+//             opacity: [
+//                 { value: .5, easing: "easeOutSine", duration: 600 },
+//                 { value: 1, easing: "easeInOutQuad", duration: 900 }],
+//
+//             filter: [
+//                 { value: "blur(5px) grayscale(100%)", easing: "easeOutSine", duration: 700 },
+//                 { value: "blur(0px) grayscale(0%)", easing: "easeInOutQuad", duration: 1000 }],
+//
+//             borderRadius: [
+//                 { value: "50%", easing: "easeOutSine", duration: 700 },
+//                 { value: "0%", easing: "easeInOutQuad", duration: 1000 }],
+//
+//             scale: [
+//                 { value: 0.5, easing: "easeOutSine", duration: 600 },
+//                 { value: 1, easing: "easeInOutQuad", duration: 1100 }],
+//
+//             delay: anime.stagger(200, { grid: [5, 5], from: index }) });
+//
+//     });
+// });
+//
+// // window.onload = () => document.querySelector('h1').remove();
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.querySelectorAll("img.stagge").forEach(img => {
+//         img.onerror = function () {
+//             this.style.display = "none";
+//         };
+//     });
+// });
 
 /*
 anime card
