c2f8875ddc5ae81614d5fc740bfdecb32cd821f6
atlas <contact@atlas-services.fr>
2020-12-21 17:06:47 +0100

    AddressRepository

---
+++ src/Repository/AddressRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Address;
+use App\Repository\Traits\BaseRepositoryTrait;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry as RegistryInterface;
+
+/**
+ * @method Address|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Address|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Address[]    findAll()
+ * @method Address[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AddressRepository extends ServiceEntityRepository
+{
+    use BaseRepositoryTrait;
+
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Address::class);
+    }
+}
