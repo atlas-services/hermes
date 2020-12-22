c0e1317a6705457866828b15b58cdba6e3e5e770
eaglod12 <billydiagne@yahoo.fr>
2020-12-22 16:05:23 +0100

    AdresseType dans dossier Ecommerce

---
+++ src/Form/Admin/Ecommerce/AddressType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form\Admin\Ecommerce;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('active')
+            ->add('locale')
+            ->add('additionalName')
+            ->add('addressLine1')
+            ->add('addressLine2')
+            ->add('administrativeArea')
+            ->add('countryCode')
+            ->add('dependentLocality')
+            ->add('familyName')
+            ->add('givenName')
+            ->add('locality')
+            ->add('organization')
+            ->add('postalCode')
+            ->add('sortingCode')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
