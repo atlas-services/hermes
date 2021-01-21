445746354cb35df15338fa360dbc21136d271cdc
atlas <contact@atlas-services.fr>
2021-01-21 19:28:34 +0100

    Tunnel de commande : présentation avec paiement stripe

--- assets/front/js/hermes/stripe.js
+++ assets/front/js/hermes/stripe.js
@@ -1,7 +1,7 @@
 // Preloader (if the #preloader div exists)
 $(window).on('load', function() {
     if ($('.stripe-button').length) {
-        $('.stripe-button-el').addClass('btn btn-outline-dark align-middle fa fa-credit-card');
+        $('.stripe-button-el').addClass('btn btn-outline-dark align-middle fa fa-credit-card mt-3');
         $('.stripe-button-el span').removeAttr('style');
         $('.stripe-button-el').removeClass('stripe-button-el');
     }
