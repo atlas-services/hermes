a74c7ff5c3af1797ee2e570d36fd79a28804dae1
atlas <contact@atlas-services.fr>
2021-01-26 20:32:15 +0100

    Tunnel de commande : réécriture/amélioration service orderclient + affichage produits

--- assets/js/react/HermesReactDeliveryFreeMessage.js
+++ assets/js/react/HermesReactDeliveryFreeMessage.js
@@ -4,9 +4,9 @@ export default class DeliveryFreeMessage extends Component {
     render() {
         let message = '';
         if (0 !=this.props.deliveryFreeLimit) {
-            message =  <div><h3>{this.props.deliveryFreeFrom}</h3></div>;
+            message =  <div><h5>{this.props.deliveryFreeFrom}</h5><hr></hr></div>;
             if (1 ==this.props.deliveryFree) {
-                message =  <div><h3><span>❤️</span> {this.props.deliveryFreeMessage} <span>❤️</span></h3></div>;
+                message =  <div><h5><span>❤️</span> {this.props.deliveryFreeMessage} <span>❤️</span></h5><hr></hr></div>;
             }
         }
         return message;
