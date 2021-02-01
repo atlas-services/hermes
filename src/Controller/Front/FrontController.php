839652a4858d317faef6166f9e6ca304d01263ac
atlas <contact@atlas-services.fr>
2021-02-01 10:45:54 +0100

    correction affichage des Témoignages en react

--- src/Controller/Front/FrontController.php
+++ src/Controller/Front/FrontController.php
@@ -161,7 +161,7 @@ class FrontController extends AbstractController
         $array = $page->getActiveMenu($sheet, $slug,$route);
         $entityManager = $this->getDoctrine()->getManager();
         $livredor = $entityManager->getRepository(Temoignage::class)->findBy(['active' => true]);
-        $array[ContactInterface::LIVREDOR] = json_encode($livredor, JSON_HEX_APOS|JSON_HEX_QUOT);
+        $array[ContactInterface::LIVREDOR] = $livredor;
 
         return $this->render('front/index.html.twig', $array);
     }
