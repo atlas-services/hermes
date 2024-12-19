c687ffacdfea51627c9b6643d31a3c6b099569e0
Tayeb <hermes@atlas-services.fr>
2024-12-19 14:22:22 +0100

    Buttons up down top et bottom disabled

--- assets/admin/js/vue/components/Section/ItemsSection.vue
+++ assets/admin/js/vue/components/Section/ItemsSection.vue
@@ -116,13 +116,13 @@ const getUpOrDown = (direction, index) => {
             <td class="col-2">{{ item.menu }}</td>
             <td class="col-1">
                 <div class="btn-group">
-                <button type="button" class="btn btn-outline-secondary " @click="changeIndex('up', index)" >
+                <button :class="{'disabled': index == 0 } " type="button" class="btn btn-outline-secondary " @click="changeIndex('up', index)" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
                 </svg> &nbsp;
                 <span class="visually-hidden">Button</span>
                 </button>
-                <button type="button" class="btn btn-outline-secondary " @click="changeIndex('down', index)">
+                <button  :class="{'disabled': index == myitems.length-1 } "  type="button" class="btn btn-outline-secondary " @click="changeIndex('down', index)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
                 </svg>
